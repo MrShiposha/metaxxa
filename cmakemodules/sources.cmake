ab57560731f81caf973de2bf1da6317e62e3a1df
Daniel Shiposha <shipdan@yandex.ru>
2018-11-03 04:52:17 +0300

    Fix sources

--- cmakemodules/sources.cmake
+++ cmakemodules/sources.cmake
@@ -2,7 +2,6 @@ SET(METAXXA_TESTS_SOURCES
     tests/main.cpp
     tests/TestFunction.h
     tests/TestMoveFunctionArgumentTypes.h
-    tests/TestReferenceWrapper.h
     tests/TestTuple.h
     tests/TestType.h
 )

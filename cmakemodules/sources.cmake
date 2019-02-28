7a34a835176ccbe84ab95113b3be2e58bc3e7651
Daniel Shiposha <shipdan@yandex.ru>
2019-03-01 01:24:01 +0300

    Remove all for GH pages

--- cmakemodules/sources.cmake
+++
@@ -1,7 +0,0 @@
-SET(METAXXA_TESTS_SOURCES
-    main.cpp
-    TestFunction.h
-    TestMoveFunctionArgumentTypes.h
-    TestTuple.h
-    TestType.h
-)
--- include/metaxxa/Macro.h
+++
@@ -1,8 +0,0 @@
-#ifndef METAXXA_detail_MACRO_H
-#define METAXXA_detail_MACRO_H
-
-#define ___METAXXA___EXPAND_DEEP(...) __VA_ARGS__
-
-#define METAXXA_EXPAND_MACRO(...) ___METAXXA___EXPAND_DEEP(__VA_ARGS__)
-
-#endif // METAXXA_detail_MACRO_H

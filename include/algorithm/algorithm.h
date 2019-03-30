1bba1080a916bafe2cce2a58e280a51b41d9ca78
Daniel Shiposha <shipdan@yandex.ru>
2019-03-30 03:28:09 +0300

    Add algorithm.h

---
+++ include/algorithm/algorithm.h
@@ -0,0 +1,8 @@
+#ifndef METAXXA_ALGORITHM_H
+#define METAXXA_ALGORITHM_H
+
+#include "indexfilter.h"
+#include "seqfilter.h"
+#include "skipfirst.h"
+
+#endif // METAXXA_ALGORITHM_H
---
+++ tests/algorithm/algorithm.cpp
@@ -0,0 +1,2 @@
+#include "indexfilter.cpp"
+#include "skipfirst.cpp"

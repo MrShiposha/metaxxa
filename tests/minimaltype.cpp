c93e7f2cb2efd33cfab4f37a62302af7c60183f7
Daniel Shiposha <shipdan@yandex.ru>
2019-04-01 04:35:07 +0300

    Fix MinimalType test

--- tests/minimaltype.cpp
+++ tests/minimaltype.cpp
@@ -4,18 +4,13 @@
         static_assert(!(sizeof(Type) <= sizeof(Type *))|| is_same_v<MinimalType<Type>, Type>, "Invalid minimal type for "#Type); \
         static_assert(!(sizeof(Type) > sizeof(Type *)) || is_same_v<MinimalType<Type>, const Type &>, "Invalid minimal type for "#Type)
 
-struct My
-{
-    int a[548];
-};
-
 TEST_CASE("[metaxxa::MinimalType]")
 {
     ___METAXXA_MINIMAL_TYPE_TEST___(char);
     ___METAXXA_MINIMAL_TYPE_TEST___(short);
     ___METAXXA_MINIMAL_TYPE_TEST___(int);
     ___METAXXA_MINIMAL_TYPE_TEST___(long long);
-    ___METAXXA_MINIMAL_TYPE_TEST___(long double);
+    ___METAXXA_MINIMAL_TYPE_TEST___(double);
 }
 
 #undef ___METAXXA_MINIMAL_TYPE_TEST___

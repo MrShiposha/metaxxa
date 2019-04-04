a93444728ef815912a2fbd13c8835b0f360c82b4
Daniel Shiposha <shipdan@yandex.ru>
2019-04-04 10:56:11 +0300

    Move catch2 to lib/catch2

---
+++ include/switch.h
@@ -0,0 +1,18 @@
+#ifndef METAXXA_SWITCH_H
+#define METAXXA_SWITCH_H
+
+namespace metaxxa
+{
+    namespace detail
+    {
+        
+    }
+
+    template <typename T, T CONSTANT>
+    struct Switch
+    {
+
+    };
+}
+
+#endif // METAXXA_SWITCH_H

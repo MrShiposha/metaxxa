763d4e9f67b329abe39d69bb76c97c83c1c1e345
Daniel Shiposha <shipdan@yandex.ru>
2019-04-04 16:30:37 +0300

    Fix TypeOrRef test

--- tests/typeorref.cpp
+++ tests/typeorref.cpp
@@ -2,7 +2,12 @@
 
 #define ___METAXXA_MINIMAL_TYPE_TEST___(Type) \
         static_assert(!(sizeof(Type) <= sizeof(Type *))|| is_same_v<TypeOrRef<Type>, Type>, "Invalid minimal type for "#Type); \
-        static_assert(!(sizeof(Type) > sizeof(Type *)) || is_same_v<TypeOrRef<Type>, const Type &>, "Invalid minimal type for "#Type)
+        static_assert(!(sizeof(Type) > sizeof(Type *)) || is_same_v<TypeOrRef<Type>, Type &>, "Invalid minimal type for "#Type)
+
+struct Bytes256
+{
+    char bytes[256];
+};
 
 TEST_CASE("[metaxxa::TypeOrRef]")
 {
@@ -11,6 +16,14 @@ TEST_CASE("[metaxxa::TypeOrRef]")
     ___METAXXA_MINIMAL_TYPE_TEST___(int);
     ___METAXXA_MINIMAL_TYPE_TEST___(long long);
     ___METAXXA_MINIMAL_TYPE_TEST___(double);
+    ___METAXXA_MINIMAL_TYPE_TEST___(Bytes256);
+
+    ___METAXXA_MINIMAL_TYPE_TEST___(const char);
+    ___METAXXA_MINIMAL_TYPE_TEST___(const short);
+    ___METAXXA_MINIMAL_TYPE_TEST___(const int);
+    ___METAXXA_MINIMAL_TYPE_TEST___(const long long);
+    ___METAXXA_MINIMAL_TYPE_TEST___(const double);
+    ___METAXXA_MINIMAL_TYPE_TEST___(const Bytes256);
 }
 
 #undef ___METAXXA_MINIMAL_TYPE_TEST___

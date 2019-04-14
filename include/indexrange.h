f2716aaec74cc8639f0ef08a72da05607a14816d
Daniel Shiposha <shipdan@yandex.ru>
2019-04-15 01:18:06 +0300

    Add MakeIndexReverseRange

--- include/indexrange.h
+++ include/indexrange.h
@@ -8,12 +8,19 @@ namespace metaxxa
     namespace detail
     {
         template <std::size_t TO_ADD, std::size_t... SEQ>
-        constexpr auto shift_seq(std::index_sequence<SEQ...> &&)
+        constexpr auto shift_seq_plus(std::index_sequence<SEQ...> &&)
             -> std::index_sequence<TO_ADD + SEQ ...>;
+
+        template <std::size_t TO_ADD, std::size_t... SEQ>
+        constexpr auto shift_seq_minus(std::index_sequence<SEQ...> &&)
+            -> std::index_sequence<TO_ADD - SEQ ...>;
     }
 
     template <std::size_t MIN, std::size_t MAX>
-    using MakeIndexRange = decltype(detail::shift_seq<MIN>(std::declval<std::make_index_sequence<MAX-MIN>>())); 
+    using MakeIndexRange = decltype(detail::shift_seq_plus<MIN>(std::declval<std::make_index_sequence<MAX-MIN>>()));
+
+    template <std::size_t MAX, std::size_t MIN>
+    using MakeReverseIndexRange = decltype(detail::shift_seq_minus<MAX - 1U>(std::declval<std::make_index_sequence<MAX-MIN>>()));
 }
 
 #endif // METAXXA_INDEXRANGE_H

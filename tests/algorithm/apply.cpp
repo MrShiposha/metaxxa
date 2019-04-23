74f58ca1c685f5c35926567dad5f2ffbb1f8fc29
Daniel Shiposha <shipdan@yandex.ru>
2019-04-23 23:57:20 +0300

    Fix apply

--- tests/algorithm/apply.cpp
+++ tests/algorithm/apply.cpp
@@ -4,7 +4,7 @@ TEST_CASE("[metaxxa::apply]")
 {
     Tuple<int, short, char> t(1, short(2), char(3));
 
-    auto r = metaxxa::apply([](auto... t) { return (0 + ... + t); }, t);
+    auto r = metaxxa::apply([](auto&... t) { return (0 + ... + t); }, t);
 
     REQUIRE(r == 6);
 }

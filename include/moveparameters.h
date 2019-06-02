931b6030995aa1b245f3915b4ec9bd5278737a91
Daniel Shiposha <shipdan@yandex.ru>
2019-06-03 01:58:25 +0300

    Add start types

--- include/moveparameters.h
+++ include/moveparameters.h
@@ -7,23 +7,28 @@ namespace metaxxa
 {
     namespace detail
     {
-        template
-        <
-            template <typename...> typename DestTemplate,
-            template <typename...> typename SrcTemplate,
-            typename... Args
-        >
-        constexpr auto move_parameters(SrcTemplate<Args...> &&) -> TypeSaver<DestTemplate<Args...>>;
+        template <typename... StartTypes>
+        struct Mover
+        {
+            template
+            <
+                template <typename...> typename DestTemplate,
+                template <typename...> typename SrcTemplate,
+                typename... Args
+            >
+            static constexpr auto move_parameters(SrcTemplate<Args...> &&) -> TypeSaver<DestTemplate<StartTypes..., Args...>>;
+        };
     }
 
     template 
     <
         template <typename...> typename DestTemplate,
-        typename SrcTemplate
+        typename SrcTemplate,
+        typename... StartTypes
     >
     using MoveParameters = typename decltype
     (
-        detail::move_parameters<DestTemplate>(std::declval<SrcTemplate>())
+        detail::Mover<StartTypes...>::move_parameters<DestTemplate>(std::declval<SrcTemplate>())
     )::Type;
 }
 
--- tests/moveparameters.cpp
+++ tests/moveparameters.cpp
@@ -2,15 +2,27 @@
 
 TEST_CASE("[metaxxa::MoveParameters]")
 {
-    using L0 = TypeList<>;
-    using L1 = TypeList<int>;
-    using L2 = TypeList<char, float>;
+    SECTION("No start types")
+    {
+        using L0 = TypeList<>;
+        using L1 = TypeList<int>;
+        using L2 = TypeList<char, float>;
 
-    using R0 = MoveParameters<TypeList, TypeTuple<>>;
-    using R1 = MoveParameters<TypeList, TypeTuple<int>>;
-    using R2 = MoveParameters<TypeList, TypeTuple<char, float>>;
+        using R0 = MoveParameters<TypeList, TypeTuple<>>;
+        using R1 = MoveParameters<TypeList, TypeTuple<int>>;
+        using R2 = MoveParameters<TypeList, TypeTuple<char, float>>;
 
-    static_assert(std::is_same_v<L0, R0>, "Invalid R0");
-    static_assert(std::is_same_v<L1, R1>, "Invalid R1");
-    static_assert(std::is_same_v<L2, R2>, "Invalid R2");
+        static_assert(std::is_same_v<L0, R0>, "Invalid R0");
+        static_assert(std::is_same_v<L1, R1>, "Invalid R1");
+        static_assert(std::is_same_v<L2, R2>, "Invalid R2");
+    }
+
+    SECTION("With start types")
+    {
+        using L = TypeList<int, char>;
+
+        using R = MoveParameters<TypeList, L, double, double>;
+
+        static_assert(std::is_same_v<R, TypeList<double, double, int, char>>);
+    }
 }
